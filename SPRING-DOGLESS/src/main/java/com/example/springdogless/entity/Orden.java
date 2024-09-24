4b0ac5746c381012284737219c68df1b77324853
alegom05 <146905500+alegom05@users.noreply.github.com>
2024-09-24 16:21:56 -0500

    correcciones

---
+++ SPRING-DOGLESS/src/main/java/com/example/springdogless/entity/Orden.java
@@ -0,0 +1,43 @@
+package com.example.springdogless.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Entity (name="ordenes")
+@Getter
+@Setter
+public class Orden {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idordenes")
+    private Integer id;
+
+    @Column
+    private String estado;
+
+    @Column
+    private Date fecha;
+
+    @Column
+    private String direccionenvio;
+
+    @Column
+    private BigDecimal total;
+
+    @Column
+    private String metodopago;
+
+    @ManyToOne
+    @JoinColumn(name = "usuariosid", nullable = false)
+    private Usuario usuario;
+
+    /*
+    @OneToMany(mappedBy = "orden", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<DetallesOrden> detallesOrden;
+    */
+}
