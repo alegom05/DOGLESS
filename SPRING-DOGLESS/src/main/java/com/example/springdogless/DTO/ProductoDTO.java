b0c5aa5a905ca549c2a01e5c540c0c0bb263cd26
alegom05 <146905500+alegom05@users.noreply.github.com>
2024-09-26 15:02:08 -0500

    Update ProductoDTO.java

--- SPRING-DOGLESS/src/main/java/com/example/springdogless/DTO/ProductoDTO.java
+++ SPRING-DOGLESS/src/main/java/com/example/springdogless/DTO/ProductoDTO.java
@@ -1,5 +1,6 @@
 package com.example.springdogless.DTO;
 
+
 public interface ProductoDTO {
     int getIdproductos();
     String getNombre();
@@ -18,4 +19,6 @@ public interface ProductoDTO {
     // Nuevos métodos para proveedor
     String getProveedorNombre();
     String getProveedorApellido();
+
+
 }
