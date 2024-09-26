55e06883647e7e37fdd635d5389f65af099733f3
FabricioEstrada <a20210795@pucp.edu.pe>
2024-09-26 10:42:38 -0500

    me kagaron

--- SPRING-DOGLESS/src/main/java/com/example/springdogless/Repository/ReposicionesRepository.java
+++ SPRING-DOGLESS/src/main/java/com/example/springdogless/Repository/ReposicionesRepository.java
@@ -5,7 +5,11 @@ import com.example.springdogless.entity.Reposicion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReposicionesRepository extends JpaRepository<Reposicion, Integer> {
+    List<Reposicion> findByAprobarIsNull(); // Método para encontrar reposiciones con aprobado nulo
+    List<Reposicion> findByAprobar(String aprobado);
 
 }
