d0377febe177757ec71c5e7e8ae950371fff9c8c
Fabricio Estrada <a20210795@pucp.edu.pe>
2024-10-03 18:07:37 -0500

    ola

--- SPRING-DOGLESS/src/main/java/com/example/springdogless/controllers/LoginController.java
+++ SPRING-DOGLESS/src/main/java/com/example/springdogless/controllers/LoginController.java
@@ -1,10 +1,27 @@
 package com.example.springdogless.controllers;
 
+import com.example.springdogless.Repository.UsuarioRepository;
+import com.example.springdogless.entity.Producto;
+import com.example.springdogless.entity.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
 
     // Mapea la vista del login
     @GetMapping("/login")
@@ -43,6 +60,34 @@ public class LoginController {
         return "politica-de-privacidad"; // Esto renderiza la vista politica-de-privacidad.html
     }
 
+    @GetMapping("/imagen/{id}")
+    @ResponseBody
+    public ResponseEntity<byte[]> getImage(@PathVariable("id") Integer id) {
+        System.out.println("Llamando a getImage con ID: " + id); // Agrega este log
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+
+        if (usuarioOptional.isPresent()) {
+            Usuario usuario = usuarioOptional.get();
+            byte[] imagenBytes = usuario.getFotoperfil();
+
+            if (imagenBytes != null) {
+                return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imagenBytes);
+            } else {
+                // Carga aquí la imagen por defecto desde el sistema de archivos
+                try {
+                    Path path = Paths.get("static/assets/img/default.jpg"); // Ajusta la ruta según la ubicación real
+                    byte[] defaultImage = Files.readAllBytes(path);
+                    return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(defaultImage);
+                } catch (IOException e) {
+                    // Manejo de errores al cargar la imagen
+                    return ResponseEntity.internalServerError().build();
+                }
+            }
+        }
+
+        System.out.println("No se encontró producto con ID: " + id); // Agrega este log
+        return ResponseEntity.notFound().build();
+    }
 
 }
 
