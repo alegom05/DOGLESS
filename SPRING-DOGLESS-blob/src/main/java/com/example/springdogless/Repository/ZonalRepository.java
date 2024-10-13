a6a3d03eea12c6f272271ae60c9fb4de5289a72d
FabricioEstrada <a20210795@pucp.edu.pe>
2024-10-13 14:50:10 -0500

    no es nada

--- SPRING-DOGLESS-blob/src/main/java/com/example/springdogless/Repository/ZonalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springdogless.Repository;
-
-import com.example.springdogless.entity.Zona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZonalRepository extends JpaRepository<Zona, Integer> {
-}
