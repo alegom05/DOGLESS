0f79b57b071fb09db5f288d402dd34f5733eb384
NiloRC <nilo.cori@pucp.edu.pe>
2024-10-14 01:06:26 -0500

    Correcciones

--- SPRING-DOGLESS/src/main/java/com/example/springdogless/controllers/AgenteController.java
+++ SPRING-DOGLESS/src/main/java/com/example/springdogless/controllers/AgenteController.java
@@ -294,9 +294,6 @@ public class AgenteController {
 
     @GetMapping(value = "/reportesOrdenes")
     public String reportesOrdenes(Model model) {
-        model.addAttribute("listaOrdenes", ordenRepository.findAll());
-
-        model.addAttribute("listaBaneados", usuarioRepository.findByRol_RolAndBorrado("Usuario",1));
 
         return "agente/reportesOrdenes";
     }
@@ -307,17 +304,29 @@ public class AgenteController {
 
         model.addAttribute("listaUsuarios", usuarioRepository.findByRol_RolAndBorrado("Usuario",1));
 
-        return "agente/reportesGuardados";
+        return "agente/reportesguardados";
     }
 
-    //Lista de órdenes por usuario
-    @GetMapping(value = "/reportePorUsuario")
-    public String ordenesPorUsuario(Model model) {
-        model.addAttribute("listaReportes", ordenRepository.findAll());
-
-        model.addAttribute("listaUsuarios", usuarioRepository.findByRol_RolAndBorrado("Usuario",1));
-
+    //----------------------seccion generación de reportes-----------------------------------------------------------------------------
+    //reporte órdenes por usuario
+    @GetMapping(value = "/reportesOrdenes/reportePorUsuario")
+    public String reporteOrdenesPorUsuario(Model model) {
         return "agente/reportePorUsuario";
+    }
+    //reporte órdenes totales
+    @GetMapping(value = "/reportesOrdenes/reporteOrdenesTotales")
+    public String reporteOrdenesTolaes(Model model) {
+        return "agente/reporteordenestotales";
+    }
+    //reporte órdenes de agentes por zona
+    @GetMapping(value = "/reportesOrdenes/reportePorAgente")
+    public String reporteOrdenesAgentesZona(Model model) {
+        return "agente/reporteporagente";
+    }
+    //reporte órdenes de agentes con filtro
+    @GetMapping(value = "/reportesOrdenes/reportePorFecha")
+    public String reporteOrdenesTotalesporFiltrofecha(Model model) {
+        return "agente/reporteporfecha";
     }
 
 
