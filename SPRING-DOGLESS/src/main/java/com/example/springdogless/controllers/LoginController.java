85e30b470a765480d69c1b488fee400417748ed2
ChristopherTerrones <106935547+ChristopherTerrones@users.noreply.github.com>
2024-11-27 20:10:34 -0500

    Formulario de registro

--- SPRING-DOGLESS/src/main/java/com/example/springdogless/controllers/LoginController.java
+++ SPRING-DOGLESS/src/main/java/com/example/springdogless/controllers/LoginController.java
@@ -1,9 +1,11 @@
 package com.example.springdogless.controllers;
 
 import com.example.springdogless.Repository.DistritoRepository;
+import com.example.springdogless.Repository.RolRepository;
 import com.example.springdogless.Repository.UsuarioRepository;
 import com.example.springdogless.dao.UsuarioDao;
 import com.example.springdogless.entity.Distrito;
+import com.example.springdogless.entity.Rol;
 import com.example.springdogless.entity.Usuario;
 import com.example.springdogless.services.EmailService;
 import jakarta.mail.internet.MimeMessage;
@@ -19,6 +21,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 import java.io.IOException;
@@ -44,6 +47,8 @@ public class LoginController {
     UsuarioDao usuarioDao;
     @Autowired
     DistritoRepository distritoRepository;
+    @Autowired
+    RolRepository rolRepository;
 
 
     // Mapea la vista del login
@@ -77,16 +82,40 @@ public class LoginController {
         return "register";
     }
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute Usuario usuario, Model model) {
+    public String registerUser(@ModelAttribute Usuario usuario, @RequestParam String confirmarContrasena, Model model) {
+        if (!usuario.getPwd().equals(confirmarContrasena)) {
+            model.addAttribute("error", "Las contraseñas no coinciden. Por favor, verifica.");
+            return "register"; // Devuelve la misma vista del formulario
+        }
+        Rol rol = rolRepository.findById(4)
+                .orElseThrow(() -> new RuntimeException("Rol no encontrado"));
+
+        // Asigna el rol predeterminado al usuario
+        usuario.setRol(rol);
         // Assuming you have a UsuarioService to handle saving the user
+        if (usuario.getDistrito() == null || usuario.getDistrito().getIddistritos() == null) {
+            throw new IllegalArgumentException("El distrito no puede ser nulo.");
+        }
 
         Distrito distrito = distritoRepository.findById(usuario.getDistrito().getIddistritos())
                 .orElseThrow(() -> new RuntimeException("Distrito not found"));
 
         usuario.setDistrito(distrito);
+        usuario.setZona(distrito.getZona());
+        usuario.setBorrado(1);  // Usuario no borrado
+        usuario.setEstado("activo");// Estado activo
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        usuario.setPwd(passwordEncoder.encode(usuario.getPwd()));
         usuarioRepository.save(usuario);
+        // Envía el correo de confirmación
+        emailService.enviarCorreo(
+                usuario.getEmail(),
+                "Confirmación de creación de cuenta",
+                "Estimado/a " + usuario.getNombre() +
+                        ",\n\nTu cuenta ha sido creada exitosamente. Ahora puedes iniciar sesión en nuestro sistema.\n\nGracias por registrarte,\nEquipo de Dogless"
+        );
         model.addAttribute("message", "Registration successful!");
-        return "redirect:/login"; // Redirect to login or another page after registration
+        return "redirect:/loginForm"; // Redirect to login or another page after registration
     }
 
     // Add this method to handle GET requests
